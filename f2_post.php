2fdc76370680f3e86e8bb4654b8a43eb59ab630a
durnevaad <93126385+durnevaad@users.noreply.github.com>
2021-11-01 14:49:12 +0500

    Упражнение 1 и 2

---
+++ f2_post.php
@@ -0,0 +1,7 @@
+﻿<b>Упражнение 3-2</b><br>
+<?php
+ echo ("Здравствуйте, " . $_POST["userName_post"]);
+ echo ("<hr>");
+ echo ("Значение скрытого поля hideField_post равно " .
+$_POST["hideField_post"]);
+?>
