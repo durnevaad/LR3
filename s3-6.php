24acd38232f455cf567f5ff667ff092d4d8e341a
durnevaad <93126385+durnevaad@users.noreply.github.com>
2021-11-06 19:20:46 +0500

    Нашла ошибку в задании, исправила

--- s3-6.php
+++ s3-6.php
@@ -3,30 +3,33 @@
 <?
 echo ("6-е задание: <br>") ;
 $text = $_POST["t"];
-$izm = str_split($text);
+$gav = strtoupper($text);
+$izm= str_split($gav);
 $s=0;
-if (in_array ("a", $izm)){
+if (in_array ("A", $izm)){
 	$s=$s+1;
 echo "Входит гласная a <br>";}
-if (in_array ("e", $izm)){
+if (in_array ("E", $izm)){
 	$s=$s+1;
 echo "Входит гласная e <br>";}
-if (in_array ("i", $izm)){
+if (in_array ("I", $izm)){
 	$s=$s+1;
 echo "Входит гласная i <br>";}
-if (in_array ("o", $izm)){
+if (in_array ("O", $izm)){
 	$s=$s+1;
 echo "Входит гласная o <br>";}
-if (in_array ("u", $izm)){
+if (in_array ("U", $izm)){
 	$s=$s+1;
 echo "Входит гласная u <br>";}
-if (in_array ("y", $izm)){
+if (in_array ("Y", $izm)){
 	$s=$s+1;
 echo "Входит гласная y <br>";}
 echo "Гласных в предложениии: $s <br>";
 
 echo ("<br>8-е задание: <br>") ;
 echo join (array_unique(str_split($text)));
+
+
 
 echo ("<br><br>10-е задание: <br>") ;
 echo strtoupper ($text);
